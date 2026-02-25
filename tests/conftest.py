3ec25f837e591c9c1b571f9579f67adb50c4d55b
Екатерина <131952002+KatrinPuzyreva-qa@users.noreply.github.com>
2026-02-25 21:14:08 +0400

    added tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,20 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from utils import attach
 
 
 @pytest.fixture(scope='function')
 def setup_browser():
+    # Устанавливаем нужные опции браузера
     options = Options()
+    options.headless = True  # Если нужен режим без графического интерфейса
+    options.add_argument('--window-size=1920,1080')  # Размер окна браузера
 
+    # Используем менеджер драйверов для автоматического выбора подходящей версии chrome
+    service = webdriver.ChromeService(ChromeDriverManager().install())
+
+    # Инициализация удалённого веб-драйвера
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options
+        command_executor="https://user1:1234@ru.selenoid.autotests.cloud/wd/hub",
+        options=options,
+        desired_capabilities={'browserName': 'chrome'}
     )
 
 
     yield driver
+    #driver.quit()  # Завершаем работу браузера после окончания теста
+
 
     # Сохраняем скриншоты, логи браузера и видео после каждого теста
     attach.add_screenshot(driver)
